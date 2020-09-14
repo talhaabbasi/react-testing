debcfbc39674c758d2cb271ddc5504a2cad9097d
Talha Abbasi <talhaabbasi1997@gmail.com>
2020-09-14 19:51:49 +0500

    Snapshot testing added

--- src/components/button/__tests__/button.test.js
+++ src/components/button/__tests__/button.test.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import Button from "./../button";
 import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import renderer from "react-test-renderer";
 
 afterEach(cleanup);
 
@@ -14,4 +15,9 @@ it("renders without crashing", () => {
 it("renders button correctly", () => {
   const { getByTestId } = render(<Button label="click me please"></Button>);
   expect(getByTestId("button")).toHaveTextContent("click me please");
+});
+
+it("matches snapshot", () => {
+  const tree = renderer.create(<Button label="save"></Button>).toJSON();
+  expect(tree).toMatchSnapshot();
 });
