7e6076e05c9b14697cba5a652f92015435f77a47
Talha Abbasi <talhaabbasi1997@gmail.com>
2020-09-14 13:11:04 +0500

    Without crashing tests added

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,5 +1,10 @@
 import React from "react";
+import "./button.css";
 
 export default function Button({ label }) {
-  return <div data-testid="button">{label}</div>;
+  return (
+    <div data-testid="button" className="button-style">
+      {label}
+    </div>
+  );
 }
