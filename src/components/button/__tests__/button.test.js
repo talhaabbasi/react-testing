088010322745fbd0bb71172bd24becbfb3bcc269
Talha Abbasi <talhaabbasi1997@gmail.com>
2020-09-14 15:21:52 +0500

    Cleanup addded

--- src/components/button/__tests__/button.test.js
+++ src/components/button/__tests__/button.test.js
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Button from "./../button";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+afterEach(cleanup);
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Button></Button>, div);
+});
+
+it("renders button correctly", () => {
+  const { getByTestId } = render(<Button label="click me please"></Button>);
+  expect(getByTestId("button")).toHaveTextContent("click me please");
 });
